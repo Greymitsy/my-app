73254359997c5108ad5236ab356ba56d684ab20c
Greymitsy <pierre.malychev@gmail.com>
2024-10-10 01:47:53 +0200

    v1.1

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,24 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
 // ... autres imports
 
 function Game() {
   // ... état et autres fonctions
 
-  const startGame = () => {
-    setIsLoading(true); // Ajoutez cet état si ce n'est pas déjà fait
-    
-    setTimeout(() => {
-      // Code pour démarrer la partie
-      setIsGameStarted(true);
-      setIsLoading(false);
-    }, 2000); // 2000 millisecondes = 2 secondes
-  };
+  // Si startGame n'est pas utilisé, supprimez-le
+  // const startGame = () => {
+  //   setIsLoading(true); // Ajoutez cet état si ce n'est pas déjà fait
+  //
+  //   setTimeout(() => {
+  //     // Code pour démarrer la partie
+  //     setIsGameStarted(true);
+  //     setIsLoading(false);
+  //   }, 2000); // 2000 millisecondes = 2 secondes
+  // };
 
   return (
     <div>
       {/* ... le reste du code pour afficher le jeu */}
+      <button onClick={handleJump}>Sauter</button>
     </div>
   );
+}
+
+// Ajoutez cette fonction pour gérer l'événement de saut
+function handleJump() {
+  // Logique pour effectuer l'action de saut
+  console.log("Sauter action déclenchée");
 }
 
 export default Game;
